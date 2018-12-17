7ca0c39bdcd4ca6faf8a60d730eca33b56eb5a69
Tawfeek Masalha <tawfeeksami@gmail.com>
2018-12-17 20:25:38 +0200

    second commit

--- src/main/java/com/tawfeek/carapp/CarApp/CarAppApplication.java
+++ src/main/java/com/tawfeek/carapp/CarApp/CarAppApplication.java
@@ -1,13 +1,20 @@
 package com.tawfeek.carapp.CarApp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarAppApplication {
 
+	  private static final Logger logger = LoggerFactory.getLogger(CarAppApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CarAppApplication.class, args);
+		
+		logger.info("Hello World");
 	}
 
 }
