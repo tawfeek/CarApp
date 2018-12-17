807ca0a07a67fc5c091de7d07fb3714681f37a7e
Tawfeek Masalha <tawfeeksami@gmail.com>
2018-12-17 19:03:25 +0200

    tawfeek first commit

---
+++ src/main/java/com/tawfeek/carapp/CarApp/CarAppApplication.java
@@ -0,0 +1,14 @@
+package com.tawfeek.carapp.CarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tawfeek/carapp/CarApp/CarAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tawfeek.carapp.CarApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
